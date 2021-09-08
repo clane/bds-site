a12f384839bc9383fc5b6db6cc94cbd24b12a025
Chris Lane <clane359@gmail.com>
2021-09-08 07:42:22 -0700

    saving

--- bdsIndex.php
+++ bdsIndex.php
@@ -5,6 +5,8 @@ require('./helpers/top.php');
 
 require('./helpers/bds_header.php'); 
 
+$html .= "<h2>About Us</h2>";
+$html .= "<p>We've been performing songs by our favorite artists since 2006 and each member has been vaccinated. This makes us a great choice for your next event. For booking, contact John at jkane49@hotmail.com or (650) 333-8014.</p>";
 	
 $html .= "<h2>Upcoming Shows</h2>";
 require('helpers/bds_schedule.php');
--- helpers/bds_header.php
+++ helpers/bds_header.php
@@ -1,6 +1,6 @@
 <?php 
 $html .=  "<header>";
 $html .= "<h1>$band_name</h1>";
-$html .= "<div>We've been doing the cover band thing since 2006 and each member has been vaccinated, making us a great choice for your next event. For booking, contact John at  jkane49@hotmail.com / (650) 333-8014.</div>";
+$html .= "<div>2006 to present</div>";
 $html .= "</header>";	
 ?>
